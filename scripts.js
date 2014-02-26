01e5daf4051f54cc1358f573dba387a43c306594
APWaldo <apw2131@columbia.edu>
2014-02-26 14:14:41 -0500

    Adding document ready to my scripts page.

--- scripts.js
+++ scripts.js
@@ -16,5 +16,8 @@ console.log ("Hi there. About to make a pretty cool visualization about Syria.")
 
 
 
+//Adding my document ready to activate my pageLoaded function (which will activate the visualization).
+$(document).ready(pageLoaded);
+
 //Putting an end page console log just for double checking things.
 console.log ("This is the end page console log. Just for double checking things.");
